886a5ac758d00ee809a624293fdcd1109359615e
yi0522 <5700351@gmail.com>
2022-06-04 00:56:16 +0800

    fix2

--- app.py
+++ app.py
@@ -36,9 +36,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='你吃飽了嗎?'))
 
 
 if __name__ == "__main__":
